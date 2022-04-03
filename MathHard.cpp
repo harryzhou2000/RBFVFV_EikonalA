01a7a5fc034632fda92770341a92c043bb306036
HarryZhou <2373256746@qq.com>
2022-04-03 20:16:32 +0800

    test2_eigen_minnorm

--- MathHard.cpp
+++ MathHard.cpp
@@ -6,6 +6,6 @@ namespace CfvMath
     void EigenLeastSquareInverse(const Eigen::MatrixXd &A, Eigen::MatrixXd &AI)
     {
         auto SVDResult = A.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV);
-        AI = SVDResult.solve(Eigen::MatrixXd::Identity(AI.cols(), AI.rows()));
+        AI = SVDResult.solve(Eigen::MatrixXd::Identity(A.rows(), A.rows()));
     }
 }
